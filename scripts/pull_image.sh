d58efc596a54c891949ac599ffeed994abb3b47b
Hoang. Tran Anh - CMCGlobal DU1 <tahoang@cmcglobal.vn>
2022-01-18 16:47:37 +0700

    up

---
+++ scripts/pull_image.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -e
+
+docker pull public.ecr.aws/i7j8m1k6/cicd:latest
---
+++ scripts/start_container.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -e
+
+docker run -d --name cmccicd public.ecr.aws/i7j8m1k6/cicd:latest
