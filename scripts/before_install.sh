99ec0f144894b11c2118eaf955c4a614dacfaef0
Hoang. Tran Anh - CMCGlobal DU1 <tahoang@cmcglobal.vn>
2022-01-18 18:30:09 +0700

    up

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,6 +3,8 @@
 #install maven
 sudo apt-get install -y maven
 
+sudo chmod 666 /var/run/docker.sock
+
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/cicd"
 if [ -d "$DIR" ]; then
